291a882a818bea2a52d3eed6993d8f5131f9883d
Viviane Santos <viviane.santos@itlean.com.br>
2021-09-21 13:33:23 -0300

    Desenvolvimento da tela Projetos e QuemSomos

---
+++ src/components/Botao/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+export default function Botao() {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.texto}>Saiba Mais</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  botao: {
+    width: 134,
+    height: 32,
+    backgroundColor: "#6854AD",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 8,
+  },
+  texto: {
+    color: "#ffffff",
+  },
+});
